0772bba3720eba4a03e7194c8785fae1d5888814
lichengjun <lichengjun@lichengjuns-MacBook-Pro.local>
2016-11-22 15:25:42 +0800

    1px in diff viewport

--- rem.js
+++ rem.js
@@ -25,7 +25,8 @@
         }
     }
 
-    var resetFontSize = function (event) {
+    //在不同的devicePixelRatio下获得相同的1px的css宽度
+    var resetFontSizeWithDevicePixelRatio = function (event) {
         var UA = window.navigator.userAgent,
             IsAndroid = /Android|HTC/i.test(UA),
             IsIPhone = !IsAndroid && /iPod|iPhone/i.test(UA),
@@ -46,6 +47,7 @@
          * iPhone手机会根据devicePixelRatio 设置viewport和font-size
          * android 手机initial-scale maximum-scale minimum-scale 默认全为1
          */
+
         if (IsAndroid || IsIPhone) {
             //可视区域宽度
             clientWidth = window.innerWidth == window.screen.availWidth ? window.screen.availWidth : window.innerWidth / devicePixelRatio;
@@ -62,7 +64,33 @@
         }
     }
 
-    bindEvent(document, 'DOMContentLoaded', resetFontSize);
-    bindEvent(window, 'resize', resetFontSize);
+    //只根据容器宽度比例设置fontSize
+    var resetFontSize = function (event) {
+        var UA = window.navigator.userAgent,
+            IsAndroid = /Android|HTC/i.test(UA),
+            IsIPhone = !IsAndroid && /iPod|iPhone/i.test(UA),
+
+            //设备devicePixelRatio  android低版本获取不到
+            devicePixelRatio = window.devicePixelRatio || 1,
+            clientWidth = 0,
+            viewport = document.getElementsByName('viewport')[0],
+            html = document.getElementsByTagName("html")[0],
+
+            baseWidth = 320, //640px base width;
+            baseDevicePixelRatio = 2;
+        //devicePixelRatio = IsIPhone ? devicePixelRatio : 1; //默认只处理iPhone手机的缩放
+
+        /*
+         *@desc reset font size for document root element
+         * max-width:640px  1rem = 100px  fontsize: 100px
+         */
+
+        document.querySelector(".wrap").style.maxWidth = baseWidth * baseDevicePixelRatio + 'px';
+        var width = document.querySelector(".wrap").clientWidth;
+        html.style.fontSize = 100 * (width / (baseWidth * baseDevicePixelRatio)) + 'px';
+    }
+
+    bindEvent(document, 'DOMContentLoaded', resetFontSizeWithDevicePixelRatio);
+    bindEvent(window, 'resize', resetFontSizeWithDevicePixelRatio);
 
 })(window);
